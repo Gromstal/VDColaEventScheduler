c60c1af9cd54ea50b07ba88174e4b4303b2b0b28
Danil Morozov <gromstal@gmail.com>
2025-05-17 22:16:09 +0300

    fix

--- src/main/java/org/example/vdcolataskscheduler/config/CacheConfig.java
+++ src/main/java/org/example/vdcolataskscheduler/config/CacheConfig.java
@@ -1,10 +1,15 @@
 package org.example.vdcolataskscheduler.config;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableCaching
@@ -12,6 +17,13 @@ public class CacheConfig {
 
     @Bean
     public CacheManager cacheManager() {
-        return new ConcurrentMapCacheManager("tasksCache");
+        CaffeineCache tasksCache = new CaffeineCache("tasksCache",
+                Caffeine.newBuilder()
+                        .expireAfterWrite(7, TimeUnit.MINUTES)
+                        .maximumSize(1000)
+                        .build());
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(List.of(tasksCache));
+        return cacheManager;
     }
 }
