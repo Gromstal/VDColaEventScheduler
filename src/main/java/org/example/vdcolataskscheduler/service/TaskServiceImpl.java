4fcb4c28e6fd046a519635bdba8cd25c81733ce6
Danil Morozov <gromstal@gmail.com>
2025-05-17 20:58:35 +0300

    fix

--- src/main/java/org/example/vdcolataskscheduler/service/TaskServiceImpl.java
+++ src/main/java/org/example/vdcolataskscheduler/service/TaskServiceImpl.java
@@ -6,29 +6,31 @@ import org.example.vdcolataskscheduler.dto.TaskDto;
 import org.example.vdcolataskscheduler.dto.UserDto;
 import org.example.vdcolataskscheduler.mapper.TaskMapper;
 import org.example.vdcolataskscheduler.mapper.UserMapper;
-import org.example.vdcolataskscheduler.repository.RedisRepository;
 import org.example.vdcolataskscheduler.repository.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.CacheManager;
 import org.springframework.stereotype.Service;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class TaskServiceImpl implements TaskService {
 
     private final TaskRepository taskRepository;
-    private final RedisRepository redisRepository;
+    private final CacheManager cacheManager;
     private final TaskMapper taskMapper;
     private final UserMapper userMapper;
     private final UserService userService;
     private static final Logger logger = LoggerFactory.getLogger(TaskServiceImpl.class);
 
+
     public void addTask(TaskDto task) {
         taskRepository.save(taskMapper.toEntity(task, userMapper.toEntity(getUser())));
-        redisRepository.delete();
+        cacheManager.getCache("tasksCache").evict("allTasks");
         logger.info("Task added successfully");
     }
 
@@ -39,33 +41,44 @@ public class TaskServiceImpl implements TaskService {
                 task.getDescription(),
                 task.getCategory(),
                 task.getDate());
-        redisRepository.delete();
+        cacheManager.getCache("tasksCache").evict("allTasks");
         logger.info("Task " + task.getId() + " updated");
     }
 
     public TaskDto findTaskById(Long id) {
-        List<TaskDto> resultList = redisRepository.getTasks();
-        if (resultList != null) {
-            return resultList
-                    .stream()
-                    .filter(task -> task.getId().equals(id))
-                    .findFirst().orElse(null);
-        }
-
-        return taskMapper.toDto(taskRepository.findById(id).orElse(null));
+        return getTasks().stream()
+                .filter(task -> task.getId().equals(id))
+                .findFirst()
+                .orElse(null);
     }
 
     public List<TaskDto> findAllTasks() {
-        List<TaskDto> resultList = redisRepository.getTasks();
-        if (resultList == null) {
-            resultList = taskMapper.toDtoList(taskRepository.findAllByUser_Id(getUser().getId()));
-            redisRepository.save(resultList);
-            resultList.sort(Comparator.comparing(TaskDto::getCreatedAt));
-        }
+        return getTasks();
+    }
 
+    private List<TaskDto> getTasks() {
+        List<TaskDto> cachedTasks = getCachedTasks();
+        return Objects.requireNonNullElseGet(cachedTasks, this::fetchAndCacheTasks);
+    }
+
+    private List<TaskDto> fetchAndCacheTasks() {
+        List<TaskDto> resultList = taskMapper.toDtoList(taskRepository.findAllByUser_Id(getUser().getId()));
+        resultList.sort(Comparator.comparing(TaskDto::getCreatedAt));
+        cacheManager.getCache("tasksCache").put("allTasks", resultList);
         logger.info("Finding all tasks for {}", getUser().getLogin());
 
         return resultList;
+    }
+
+    private List<TaskDto> getCachedTasks() {
+        var cache = cacheManager.getCache("tasksCache");
+        if (cache != null) {
+            var wrapper = cache.get("allTasks");
+            if (wrapper != null) {
+                return (List<TaskDto>) wrapper.get();
+            }
+        }
+        return null;
     }
 
     private UserDto getUser() {
